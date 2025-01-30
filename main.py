37dc15a7f90ef75db6d3271f5b894bf896b0b6f8
Anitha-Balachandran <anitha.balachandran@sjsu.edu>
2025-01-30 14:34:55 -0800

    Model trainer, hyperparameter tuning and pipeline completed

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ from networksecurity.entity.config_entity import (
 
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
-# from networksecurity.components.model_trainer import ModelTrainer
-# from networksecurity.entity.config_entity import ModelTrainerConfig
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
 
 
 import sys
@@ -46,7 +46,7 @@ if __name__ == "__main__":
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
 
-        """logging.info("Model Training sstared")
+        logging.info("Model Training sstared")
         model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
         model_trainer = ModelTrainer(
             model_trainer_config=model_trainer_config,
@@ -54,7 +54,7 @@ if __name__ == "__main__":
         )
         model_trainer_artifact = model_trainer.initiate_model_trainer()
 
-        logging.info("Model Training artifact created")"""
+        logging.info("Model Training artifact created")
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
