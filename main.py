8d0c48b7e0de6dbada0b60244a29879e7953f70f
Anitha-Balachandran <anitha.balachandran@sjsu.edu>
2025-01-30 12:48:08 -0800

    Updated data validation and transformation logic

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from networksecurity.components.data_ingestion import DataIngestion
 
-# from networksecurity.components.data_validation import DataValidation
-# from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.data_validation import DataValidation
+
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import (
+    DataIngestionConfig,
+    DataValidationConfig,
+    DataTransformationConfig,
+)
 
-# DataValidationConfig,DataTransformationConfig
+
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 # from networksecurity.components.model_trainer import ModelTrainer
@@ -24,7 +29,7 @@ if __name__ == "__main__":
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
-        """data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
         data_validation = DataValidation(dataingestionartifact, data_validation_config)
         logging.info("Initiate the data Validation")
         data_validation_artifact = data_validation.initiate_data_validation()
@@ -41,7 +46,7 @@ if __name__ == "__main__":
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
 
-        logging.info("Model Training sstared")
+        """logging.info("Model Training sstared")
         model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
         model_trainer = ModelTrainer(
             model_trainer_config=model_trainer_config,
