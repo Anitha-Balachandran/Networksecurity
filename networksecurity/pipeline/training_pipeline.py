99912012197fc9b4baf9c4333a6cf256426c777d
Anitha-Balachandran <anitha.balachandran@sjsu.edu>
2025-01-30 23:26:16 -0800

    Update model pusher to aws s3

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -26,7 +26,7 @@ from networksecurity.entity.artifact_entity import (
 
 from networksecurity.constant.training_pipeline import TRAINING_BUCKET_NAME
 
-# from networksecurity.cloud.s3_syncer import S3Sync
+from networksecurity.cloud.s3_syncer import S3Sync
 from networksecurity.constant.training_pipeline import SAVED_MODEL_DIR
 import sys
 
@@ -34,7 +34,7 @@ import sys
 class TrainingPipeline:
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
-        #self.s3_sync = S3Sync()
+        self.s3_sync = S3Sync()
 
     def start_data_ingestion(self):
         try:
@@ -143,9 +143,9 @@ class TrainingPipeline:
             model_trainer_artifact = self.start_model_trainer(
                 data_transformation_artifact=data_transformation_artifact
             )
-            """
+
             self.sync_artifact_dir_to_s3()
-            self.sync_saved_model_dir_to_s3()"""
+            self.sync_saved_model_dir_to_s3()
 
             return model_trainer_artifact
         except Exception as e:
